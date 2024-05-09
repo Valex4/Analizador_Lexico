7df53a965e45de69444e4d29cc9e5a62793e2824
Valex4 <yam778123@gmail.com>
2024-05-09 11:44:01 -0600

    first commit

--- app/settings.py
+++ app/settings.py
@@ -125,4 +125,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Esta es la ubicación de tus archivos estáticos dentro de tu aplicación
+]
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
