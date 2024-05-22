19cabd127b0064083332a3076f5e92e443e4682b
Valex4 <yam778123@gmail.com>
2024-05-22 09:15:06 -0600

    [ADD] Examen

--- analizador_lexico_V2/urls.py
+++ analizador_lexico_V2/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, re_path
-from .views import home
+from .views import home, examen
 
 
 urlpatterns = [
     path('V2/', home, name='home'),
+    path('V2/examen', examen, name='examen' )
 ]
--- analizador_lexico_V2/views.py
+++ analizador_lexico_V2/views.py
@@ -153,4 +153,39 @@ def home(request):
 
 
 
+def calcular_total(productos):
+    iva = 0.16  # 16% de IVA
+    resultados = []
+
+    for nombre_producto, precio in productos:
+        iva_monto = precio * iva
+        total = precio + iva_monto
+        resultado = {
+            'nombre': nombre_producto,
+            'precio': precio,
+            'iva': iva_monto,
+            'total': total
+        }
+        resultados.append(resultado)
+
+    return resultados
+
+
+def examen(request):
+    resultado = []
+
+    if request.method == 'POST':
+        productos = []
+        for nombre in request.POST.getlist('nombre'):
+            precio_str = request.POST.get(f'precio_{len(productos)}')
+            if precio_str:
+                precio = float(precio_str)
+                productos.append((nombre, precio))
+
+        resultado = calcular_total(productos)
+
+    return render(request, 'static/Examen.html', {'resultados': resultado})
+
+
+
  
